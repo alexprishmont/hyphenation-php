40d21cbf60d4de3581b9f883a4a51d402f7bc1bb
Alex Prismont <aprishmont@gmail.com>
2019-07-03 11:20:52 +0300

    Added comments, fixed code alignment

--- scan.php
+++ scan.php
@@ -1,10 +1,14 @@
 <?php
-    function getTextFileData($src) {
-        $file = new SplFileObject($src);
-        $data = [];
-        foreach($file as $data_num => $data_c) {
-            $data[] = $data_c;
-        }
-        return $data;
+/**
+ * @param $src
+ * @return array
+ */
+function getTextFileData($src) {
+    $file = new SplFileObject($src);
+    $data = [];
+    foreach($file as $data_num => $data_c) {
+        $data[] = $data_c;
     }
+    return $data;
+}
 
