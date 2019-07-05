4565f1ba7ff74d41f4dc606aafa854b7ccce9e3e
Alex Prismont <aprishmont@gmail.com>
2019-07-05 12:14:09 +0300

    Startup file refactored

--- Core/Main.php
+++ Core/Main.php
@@ -23,6 +23,22 @@ class Main
         $this->string_algorithm = new Stringhyphenation($this->word_algorithm);
     }
 
+    public function startup(array $argv, int $argc): void
+    {
+        if ($argc > 3 || $argc <= 2) {
+            echo "Your entered arguments are wrong!\n";
+            echo "Usage:\n $argv[0] -w [word]\n $argv[0] -s [sentence]\n $argv[0] -f [file location]\n";
+            echo "For email validation use: $argv[0] -email [you_email]\n";
+        } else {
+            if (isset($argv[1]) && isset($argv[2])) {
+                $option = $argv[1];
+                $target = $argv[2];
+
+                $this->load_algorithm($option, $target);
+            }
+        }
+    }
+
     public function load_algorithm(string $option, string $target): void
     {
         $start_timing = microtime(true);
