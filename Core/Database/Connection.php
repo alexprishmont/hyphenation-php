b7987893565770d2b44d9c7f5d7ac540f9246d1f
Alex Prismont <aprishmont@gmail.com>
2019-07-16 09:57:00 +0300

    database fixes

--- Core/Database/Connection.php
+++ Core/Database/Connection.php
@@ -15,8 +15,6 @@ use \PDO;
 class Connection implements DatabaseInterface
 {
     private $handle;
-    private $dsn;
-    private $options;
 
     private $logger;
     private $cache;
@@ -34,20 +32,20 @@ class Connection implements DatabaseInterface
             Tools::CACHE_FILE_MODE
         );
 
-        $this->dsn = "mysql:host=" . DatabaseSettings::get()['host'] .
+        $dsn = "mysql:host=" . DatabaseSettings::get()['host'] .
             ";dbname=" . DatabaseSettings::get()['database'] .
             ";charset=" . DatabaseSettings::get()['charset'];
 
-        $this->options = [
+        $options = [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES => false,
         ];
 
-        $this->handle = new PDO($this->dsn,
+        $this->handle = new PDO($dsn,
             DatabaseSettings::get()['user'],
             DatabaseSettings::get()['password'],
-            $this->options);
+            $options);
     }
 
     public function getHandle(): object
