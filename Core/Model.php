384eca90afe2b8db24b7dd351b047e9f3f9f8144
Alex Prismont <aprishmont@gmail.com>
2019-07-25 08:44:52 +0300

    Little class refactor

--- Core/Model.php
+++ Core/Model.php
@@ -15,4 +15,25 @@ class Model
         $this->builder = QueryBuilder::getInstanceOf();
         $this->connectionHandle = Singleton::getInstanceOf();
     }
+
+    public function count()
+    {
+    }
+
+    public function find()
+    {
+    }
+
+    public function read()
+    {
+    }
+
+    public function create()
+    {
+    }
+
+    public function delete()
+    {
+    }
+
 }
