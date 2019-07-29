fb1583e907d7ac7265b1b0fc65bc14853798f94d
Alex Prismont <aprishmont@gmail.com>
2019-07-29 11:50:46 +0300

    small fix

--- app/Core/Input/Validator.php
+++ app/Core/Input/Validator.php
@@ -34,7 +34,7 @@ class Validator implements ValidatorInterface
     {
         if (!isset($input[2]) || !isset($input[1])) {
             throw new InvalidFlagException(
-                'Usage: php bin/' . $input[0] . ' ' . '[flag] [target]');
+                'Usage: php ' . $input[0] . ' ' . '[flag] [target]');
         }
 
         $method = $input[1];
@@ -44,14 +44,14 @@ class Validator implements ValidatorInterface
             case '-reset':
                 if ($target !== 'cache') {
                     throw new InvalidFlagException(
-                        'You can reset only cache. php bin/' . $input[0] . ' ' . $input[1] . ' cache'
+                        'You can reset only cache. php ' . $input[0] . ' ' . $input[1] . ' cache'
                     );
                 }
                 break;
             case '-import':
                 if ($target !== 'patterns') {
                     throw new InvalidFlagException(
-                        'You can only import patterns. php bin/' . $input[0] . ' ' . $input[1] . ' patterns'
+                        'You can only import patterns. php ' . $input[0] . ' ' . $input[1] . ' patterns'
                     );
                 }
                 break;
