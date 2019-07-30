c8080dc852493a1eb5ddaa56b12e99993227deff
Alex Prismont <aprishmont@gmail.com>
2019-07-30 09:04:21 +0300

    Fixed Config class, tests for Config

--- app/Core/Config.php
+++ app/Core/Config.php
@@ -3,15 +3,26 @@ declare(strict_types=1);
 
 namespace NXT\Core;
 
+use NXT\Core\Exceptions\InvalidFlagException;
+
 class Config
 {
     public function get(string $configName): array
     {
         $path = $this->getConfigPath($configName);
+
+        if (!$this->isConfigExists($path)) {
+            throw new InvalidFlagException('Config [' . $path . '] does not exist.');
+        }
+
         $config = json_decode(
             file_get_contents($path),
             true
         );
+
+        if (!isset($config) || empty($config)) {
+            throw new InvalidFlagException('Config file is empty. Cannot load..');
+        }
         return $config;
     }
 
@@ -21,6 +32,11 @@ class Config
             $this->getConfigPath($configName),
             json_encode($config)
         );
+    }
+
+    private function isConfigExists(string $path): bool
+    {
+        return file_exists($path);
     }
 
     private function getConfigPath(string $name): string
--- tests/unit/ConfigLoaderTest.php
+++ tests/unit/ConfigLoaderTest.php
@@ -2,6 +2,8 @@
 declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
+use NXT\Core\Config;
+use NXT\Core\Exceptions\InvalidFlagException;
 
 class ConfigLoaderTest extends TestCase
 {
@@ -13,9 +15,10 @@ class ConfigLoaderTest extends TestCase
         'OUTPUT_SRC' => '/output',
         'CACHE_OUTPUT_SRC' => '/cache'
     ];
+
     protected function setUp(): void
     {
-        $this->config = new \NXT\Core\Config();
+        $this->config = new Config();
     }
 
     protected function tearDown(): void
@@ -39,5 +42,21 @@ class ConfigLoaderTest extends TestCase
         $testCase = $this->config->get('config');
 
         $this->assertEquals($newTestcase, $testCase);
+    }
+
+    public function testTryReadEmptyConfig(): void
+    {
+        $this->expectException(InvalidFlagException::class);
+
+        $newTestcase = [];
+        $this->config->set('config', $newTestcase);
+        $this->config->get('config');
+    }
+
+    public function testTryReadNotExistingConfig(): void
+    {
+        $this->expectException(InvalidFlagException::class);
+
+        $this->config->get('notexistingConfig');
     }
 }
